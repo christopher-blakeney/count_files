459e763cfe0899d4159331d1f82027302d8511b9
Christopher Blakeney <christopherjblakeney@gmail.com>
2023-04-20 12:38:08 +1000

    almost done, all functions work just need to output to .txt file

--- count_files.py
+++ count_files.py
@@ -5,57 +5,62 @@ __version__ = "0.1.0"
 __license__ = ""
 
 
-def count_files(path, excl_dirpath, show_hidden=False):
+def print_to_txt(path, output):
+    with open(f"{path}/directory_summary.txt", "w") as f:
+        f.write(str(output))
+
+
+def count_files(path, exclude_hidden=False, show_files=True):
     n = 0
-    hid = 0
+    hidden_files = 0
+    t_hidden_files = 0
     for dirpath, dirname, filenames in os.walk(path):
-        # if show hidden false, subtract files beginning with "." from c
-        if show_hidden == False:
+        # count hidden files for each dir
+        for f in filenames:
+            if f[0] == ".":
+                hidden_files += 1
+                t_hidden_files += 1
+        # if show hidden false, subtract files and dirs beginning with "." from the filenames and dirname list
+        if exclude_hidden == True:
             for f in filenames:
                 filenames = [f for f in filenames if not f[0] == "."]
-                dirname[:] = [d for d in dirname if not d[0] == "."]
-            c = len(filenames)
-        # if show hidden true, c is length of files
-        else:
-            c = len(filenames)
-        # if dirpath is excluded, add excluded flag and minus from total
-        if dirpath in excl_dirpath:
-            n = n - c + hid
-            print(
-                dirpath,
-                "\n>> [EXCLUDED] ",
-                c,
-                " files present | ",
-                hid,
-                " hidden | ",
-                c + hid,
-                " total\n",
-            )
-        else:
-            n += c
-            print(
-                dirpath,
-                "\n>> ",
-                c,
-                " files present | ",
-                hid,
-                " hidden | ",
-                c + hid,
-                "total" "\n                ",
-            )
-    print(n, " files in path", hid, " hidden files")
+
+        c = len(filenames) - hidden_files
+        n += c
+        dir_output = (
+            dirpath,
+            "\n>> ",
+            c,
+            " real files | ",
+            hidden_files,
+            " hidden | ",
+            c + hidden_files,
+            "total\n",
+        )
+
+        if show_files == True:
+            for i in filenames:
+                print("        - ", i)
+            print("\n")
+
+        # reset hidden files for next dir in loop
+        hidden_files = 0
+    total_output = print(
+        "\nTOTALS: ",
+        n,
+        " real files | ",
+        t_hidden_files,
+        " hidden | ",
+        n + t_hidden_files,
+        " total\n",
+    )
+    return dir_output, total_output
 
 
 def main():
     # replace with path to be counted
     path = "/Users/christopher/Desktop/Peter Mac/Penny Ad Hoc/Pubs_found/Penny_pubs"
-    count_files(
-        path,
-        # replace with paths to be excluded from total counts
-        excl_dirpath=[
-            "/Users/christopher/Desktop/Peter Mac/Penny Ad Hoc/Pubs_found/Penny_pubs/removed"
-        ],
-    )
+    print_to_txt(path, count_files(path))
 
     """
     usage = '\nReturn summary count of given directory.\nUsage: count_files.py [path]\n'
