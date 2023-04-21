2a2cfc0f75969bb16fa26ccf4ed4cc9e120bd063
Christopher Blakeney <christopherjblakeney@gmail.com>
2023-04-21 11:30:17 +1000

    done. fixed output to txt file, all functionality seems stable.

--- count_files.py
+++ count_files.py
@@ -1,16 +1,13 @@
 import os
+import re
 
 __author__ = "Christopher J. Blakeney"
 __version__ = "0.1.0"
 __license__ = ""
 
 
-def print_to_txt(path, output):
-    with open(f"{path}/directory_summary.txt", "w") as f:
-        f.write(str(output))
-
-
 def count_files(path, exclude_hidden=False, show_files=True):
+    txt_file = open(f"{path}/directory_summary.txt", "w")
     n = 0
     hidden_files = 0
     t_hidden_files = 0
@@ -25,42 +22,34 @@ def count_files(path, exclude_hidden=False, show_files=True):
             for f in filenames:
                 filenames = [f for f in filenames if not f[0] == "."]
 
+        # find end directory with regex to print
+        match = re.search("/(\w+$)", dirpath)
+        subdir = match.group(1)
+
         c = len(filenames) - hidden_files
         n += c
-        dir_output = (
-            dirpath,
-            "\n>> ",
-            c,
-            " real files | ",
-            hidden_files,
-            " hidden | ",
-            c + hidden_files,
-            "total\n",
+        txt_file.write(
+            f"\nPATH: {dirpath} \nSUB-DIRECTORY: {subdir}\n\n    >> {c} real files | {hidden_files} hidden | {c + hidden_files} total\n"
         )
 
         if show_files == True:
             for i in filenames:
-                print("        - ", i)
-            print("\n")
+                txt_file.write(f"\n        - {i}")
+            txt_file.write("\n")
 
         # reset hidden files for next dir in loop
         hidden_files = 0
-    total_output = print(
-        "\nTOTALS: ",
-        n,
-        " real files | ",
-        t_hidden_files,
-        " hidden | ",
-        n + t_hidden_files,
-        " total\n",
+    txt_file.write(
+        f"\nTOTALS: {n} real files | {t_hidden_files} hidden | {n + t_hidden_files} total\n"
     )
-    return dir_output, total_output
+    txt_file.close()
+    print("\nSuccess! Summary file placed in directory.\n")
 
 
 def main():
     # replace with path to be counted
     path = "/Users/christopher/Desktop/Peter Mac/Penny Ad Hoc/Pubs_found/Penny_pubs"
-    print_to_txt(path, count_files(path))
+    count_files(path)
 
     """
     usage = '\nReturn summary count of given directory.\nUsage: count_files.py [path]\n'
